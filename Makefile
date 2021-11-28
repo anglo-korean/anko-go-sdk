9e81b63cb615ffb5d80edd7459ad73d5b8875b28
jspc <james+git@zero-internet.org.uk>
2021-11-28 15:17:01 +0000

    Auto-generate README

--- Makefile
+++ Makefile
@@ -2,3 +2,6 @@ DOCUMENTATION_CHECKOUT ?= ../documentation
 
 gateway.pb.go gateway_grpc.pb.go: $(DOCUMENTATION_CHECKOUT)/proto/gateway.proto
 	protoc -I $(dir $<) $< --go_out=module=github.com/anglo-korean/anko-go-sdk:. --go-grpc_out=module=github.com/anglo-korean/anko-go-sdk:.
+
+README.md: doc.go
+	goreadme -import-path github.com/anglo-korean/anko-go-sdk -title github.com/anglo-korean/anko-go-sdk -badge-godoc -badge-goreportcard -skip-sub-packages > $@
--- doc.go
+++ doc.go
@@ -1,6 +1,9 @@
 // Package anko provides a golang SDK to the Anko Investor Forecasts gRPC service.
+//	import "github.com/anglo-korean/anko-go-sdk"
 //
 // It handles authentication and reconnection logic.
+//
+//	anko.ConnectionTimeout = time.Second // default: 5s
 //
 //	client, err := anko.New("anko-1234", "my-client")
 //	if err != nil {
@@ -14,4 +17,13 @@
 //
 //
 // This SDK accepts an Anko Token and a per-connection name (which may aid debugging where a single token is used across auto-scaled services) and returns a channel of forecasts for ready consumption.
+//
+// client.Handle accepts an anko.Handler function, a convention borrowed from packages such as net/http.
+// This function looks like:
+//	type Handler func(*anko.Forecast) error
+//
+// This function will be called on every Forecast received from the Anko Investor Forecasts gRPC Service.
+// Any returned error will halt the processing of Forecasts. By default these functions are called sequentially; this is the opposite behaviour to http.HandlerFunc, where each handler runs in a new go function.
+//
+// If this behaviour is important to you, then simply write your anko.Handler to provide its own sync/ dispatch/ gofunc logic.
 package anko
